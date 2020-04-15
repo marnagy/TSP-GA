2f5e5ea37bf02c457ec7bd170d15a743f06d7132
Marek Nagy <62247071@cuni.cz>
2020-04-15 11:23:34 +0200

    Add project files.

---
+++ TSP/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace TSP
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.generationsCounter = new System.Windows.Forms.Label();
+			this.loadButton = new System.Windows.Forms.Button();
+			this.button1 = new System.Windows.Forms.Button();
+			this.trackBar1 = new System.Windows.Forms.TrackBar();
+			this.trainNum = new System.Windows.Forms.Label();
+			this.showTimer = new System.Windows.Forms.Timer(this.components);
+			this.popIndexLabel = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+			this.pictureBox1.Location = new System.Drawing.Point(12, 32);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(729, 583);
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
+			// 
+			// label1
+			// 
+			this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+			this.label1.Location = new System.Drawing.Point(803, 41);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(53, 20);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Points";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.textBox1.Location = new System.Drawing.Point(807, 66);
+			this.textBox1.Margin = new System.Windows.Forms.Padding(5);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+			this.textBox1.Size = new System.Drawing.Size(220, 20);
+			this.textBox1.TabIndex = 2;
+			this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+			// 
+			// generationsCounter
+			// 
+			this.generationsCounter.AutoSize = true;
+			this.generationsCounter.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+			this.generationsCounter.Location = new System.Drawing.Point(803, 612);
+			this.generationsCounter.Name = "generationsCounter";
+			this.generationsCounter.Size = new System.Drawing.Size(51, 20);
+			this.generationsCounter.TabIndex = 3;
+			this.generationsCounter.Text = "label2";
+			// 
+			// loadButton
+			// 
+			this.loadButton.Location = new System.Drawing.Point(807, 94);
+			this.loadButton.Name = "loadButton";
+			this.loadButton.Size = new System.Drawing.Size(220, 23);
+			this.loadButton.TabIndex = 4;
+			this.loadButton.Text = "Load";
+			this.loadButton.UseVisualStyleBackColor = true;
+			this.loadButton.Click += new System.EventHandler(this.loadButton_Click);
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(807, 157);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(220, 23);
+			this.button1.TabIndex = 5;
+			this.button1.Text = "Start";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// trackBar1
+			// 
+			this.trackBar1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.trackBar1.Location = new System.Drawing.Point(807, 241);
+			this.trackBar1.Maximum = 300;
+			this.trackBar1.Minimum = 20;
+			this.trackBar1.Name = "trackBar1";
+			this.trackBar1.Size = new System.Drawing.Size(220, 45);
+			this.trackBar1.TabIndex = 6;
+			this.trackBar1.Value = 20;
+			this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+			// 
+			// trainNum
+			// 
+			this.trainNum.AutoSize = true;
+			this.trainNum.Location = new System.Drawing.Point(807, 222);
+			this.trainNum.Name = "trainNum";
+			this.trainNum.Size = new System.Drawing.Size(0, 13);
+			this.trainNum.TabIndex = 7;
+			// 
+			// showTimer
+			// 
+			this.showTimer.Tick += new System.EventHandler(this.timer1_Tick);
+			// 
+			// popIndexLabel
+			// 
+			this.popIndexLabel.AutoSize = true;
+			this.popIndexLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+			this.popIndexLabel.Location = new System.Drawing.Point(12, 9);
+			this.popIndexLabel.Name = "popIndexLabel";
+			this.popIndexLabel.Size = new System.Drawing.Size(0, 20);
+			this.popIndexLabel.TabIndex = 8;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1130, 644);
+			this.Controls.Add(this.popIndexLabel);
+			this.Controls.Add(this.trainNum);
+			this.Controls.Add(this.trackBar1);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.loadButton);
+			this.Controls.Add(this.generationsCounter);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.pictureBox1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label generationsCounter;
+		private System.Windows.Forms.Button loadButton;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TrackBar trackBar1;
+		private System.Windows.Forms.Label trainNum;
+		private System.Windows.Forms.Timer showTimer;
+		private System.Windows.Forms.Label popIndexLabel;
+	}
+}
+
---
+++ TSP/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TSP
+{
+	public partial class Form1 : Form
+	{
+
+		Bitmap bmp;
+		Graphics g;
+		Brush bgBrush = new SolidBrush(Color.Black);
+		Brush pointBrush = new SolidBrush(Color.White);
+		Brush stringBrush = new SolidBrush(Color.Red);
+		Pen linePen = new Pen(Color.Blue);
+		//Color lineColor = Color.Blue;
+		Pen bestLinePen = new Pen(Color.Green);
+		//Color bestColor = Color.Green;
+		int genCounter = 0;
+		Point first;
+		Point[] points;
+		int pointWidth = 5;
+		Rectangle[] pointRect;
+		Random rand;
+		int pointMargin = 10;
+		bool train = false;
+		Point[][] population;
+		int populationLengthMultiplier = 10;
+		int sleepAmount = 1000/60;
+		int showTimerIndex = 0;
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+			g = Graphics.FromImage(bmp);
+			pictureBox1.Image = bmp;
+
+			ClearPictureBox();
+			pictureBox1.Refresh();
+			generationsCounter.Text = "Generation: " + genCounter;
+			trainNum.Text = "Generations to train: " + trackBar1.Value;
+			rand = new Random();
+		}
+		private void ClearPictureBox()
+		{
+			g.FillRectangle(bgBrush, 0,0,pictureBox1.Width, pictureBox1.Height);
+		}
+
+		private void loadButton_Click(object sender, EventArgs e)
+		{
+			int numberOfPoints;
+			if ( int.TryParse(textBox1.Text, out numberOfPoints) && numberOfPoints > 1)
+			{
+				first = NewRandPoint();
+				points = new Point[numberOfPoints - 1];
+
+				for (int i = 0; i < points.Length; i++)
+				{
+					points[i] = NewRandPoint();
+				}
+			}
+			MakePointRects();
+
+			ClearPictureBox();
+			ShowPoints();
+			pictureBox1.Refresh();
+		}
+
+		private void MakePointRects()
+		{
+			pointRect = new Rectangle[points.Length + 1];
+			pointRect[points.Length] = new Rectangle(first.X - pointWidth, first.Y - pointWidth, pointWidth * 2, pointWidth * 2);
+			for (int i = 0; i < points.Length; i++)
+			{
+				pointRect[i] = new Rectangle(points[i].X - pointWidth, points[i].Y - pointWidth, pointWidth * 2, pointWidth * 2);
+			}
+		}
+
+		private void ShowPoints()
+		{
+			for (int i = 0; i < pointRect.Length; i++)
+			{
+				g.FillEllipse(pointBrush, pointRect[i]);
+			}
+		}
+
+		private Point NewRandPoint()
+		{
+			return new Point(rand.Next(pointMargin, pictureBox1.Width - pointMargin), rand.Next(pointMargin, pictureBox1.Height - pointMargin));
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			train = true;
+			population = new Point[(points.Length + 1) * populationLengthMultiplier][];
+
+			for (int i = 0; i < population.Length; i++)
+			{
+				population[i] = Shuffle(points);
+			}
+
+			showTimerIndex = 0;
+			showTimer.Start();
+			//Thread.Sleep(population.Length * 150);
+
+			// start training
+		}
+
+		private void ShowSolution(Point[] sol, int index)
+		{
+			ClearPictureBox();
+			ShowPoints();
+
+			int i = 0;
+			g.DrawLine(linePen, first, sol[i]);
+			for (i = 0; i < sol.Length - 1; i++)
+			{
+				g.DrawLine(linePen, sol[i], sol[i+1]);
+			}
+			g.DrawLine(linePen, sol[sol.Length - 1], first);
+			
+			pictureBox1.Refresh();
+		}
+
+		private Point[] Shuffle(Point[] points)
+		{
+			Point[] res = new Point[points.Length];
+			for (int i = 0; i < points.Length; i++)
+			{
+				res[i] = points[i];
+			}
+
+			// shuffle alg
+			Point temp;
+			int index;
+			int index2;
+			for (int i = 0; i < res.Length; i++)
+			{
+				//if (i > 0)
+				//{
+				index = rand.Next(0,res.Length - 1);
+				index2 = rand.Next(0,res.Length - 1);
+				temp = res[index2];
+				res[index2] = res[index];
+				res[index] = temp;
+				//}
+			}
+			return res;
+		}
+
+		private void trackBar1_Scroll(object sender, EventArgs e)
+		{
+			trainNum.Text = "Generations to train: " + trackBar1.Value;
+		}
+
+		private void timer1_Tick(object sender, EventArgs e)
+		{
+			if (showTimerIndex >= 0 && showTimerIndex < population.Length)
+			{
+				ShowSolution(population[showTimerIndex], showTimerIndex);
+				popIndexLabel.Text = "Population index: " + (showTimerIndex + 1);
+				pictureBox1.Refresh();
+				showTimerIndex++;
+			}
+			else
+			{
+				showTimer.Stop();
+			}
+		}
+	}
+}
